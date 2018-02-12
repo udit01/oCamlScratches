e4533ec4175cf66aff454bb8748628d3a2e7b54e
udit01 <uditjainiitd@gmail.com>
2018-02-12 16:48:39 +0530

    some bug in tb

--- A2/Testbench.ml
+++ A2/Testbench.ml
@@ -6,7 +6,7 @@ let gamma v = match v with
                         |"y" -> AnswerInt(5)
                         | "true" -> AnswerBool(true)
                         | "false" -> AnswerBool(false)
-                        | "random" -> AnswerInt(11)
+                        | "random" -> AnswerBool(true)
                         |_ -> AnswerInt(2) )
         | _ -> AnswerInt(1)                
         
@@ -31,13 +31,13 @@ let g6 = AnswerInt(5)
 let e7 = Pow (e5,e6)
 let g7 = AnswerInt(100000)
 
-let e8 = EqI(Sub(Add(Const(2),Const(3)), Const(5)),Const(0))
+let e8 = EqI(Sub(Add(Const(2),Const(33)), Const(35)),Const(0))
 let g8 = AnswerBool(true)
 
-let e9 = Div( Mul(Pow(Const(2), Const(10)), Const(4)), Const(1024) )
+let e9 = Div( Mul(Pow(Const(5), Const(3)), Const(4)), Const(125) )
 let g9 = AnswerInt(4)
 
-let e10 = Impl(Gt(Add(Var (S "x"), Const(80)), Const(80)), Var( S "true"))
+let e10 = Impl(Gt(Add(Var (S "x"), Const(100)), Const(50)), Var( S "true"))
 let g10 = AnswerBool(true)
 
 let e11 = Not(Or(e8, Var (S "random")))
@@ -65,10 +65,11 @@ let gt = [g1;g2;g3;g4;g5;g6;g7;g8
         ];;
 
 let rec checker i (l , gtr) = match (l,gtr) with
-        ([],[]) -> ()
+        ([],[]) -> (Printf.printf "Checking done for %d values.\n" i ) 
         | (x::xs,y::ys) -> ((if((execute ([],gamma,(compile x))) <> y ) then (Printf.printf "Execute(compile) =/= ground Truth for: %d\n " i) 
-                        else if ((eval gamma x) <> y ) then (Printf.printf "Eval =/= ground Truth for %d\n: " i)
-                        else (Printf.printf "Success for: %d\n" i ) ) ; checker (i+1) (xs,ys) );;
+                        else if ((eval gamma x) <> y ) then (Printf.printf "Eval =/= ground Truth for %d\n " i)
+                        else (Printf.printf "Success for: %d\n" i ) ) ; checker (i+1) (xs,ys) )
+        (* | _ ->    (Printf.printf "Error in positioning of lables and input data : %d\n" i )             *)
 
 
 checker 0 (l,gt);;
