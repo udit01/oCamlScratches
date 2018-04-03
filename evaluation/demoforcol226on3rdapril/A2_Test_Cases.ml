dde6c45fca025982cbd5d3e2d1b364e552cab7ec
udit01 <uditjainiitd@gmail.com>
2018-04-03 10:12:00 +0530

    finalized example cases for A2

--- evaluation/demoforcol226on3rdapril/A2_Test_Cases.ml
+++ evaluation/demoforcol226on3rdapril/A2_Test_Cases.ml
@@ -1,3 +1,4 @@
+(* #use "DefInterpreter.ml" *)
 (* Note:
  * Please make suitable changes to the shared test cases so that
  * the constructors match your signature definition.
@@ -5,42 +6,54 @@
 
 (*--==Compile & Execute==--*)
 
-Add(Const(1),Const(2));;
-Mul(Const(6),Const(6));;
-Pow(Const(2),Const(4));;
-Div(Const(6),Const(3));;
-Var(S "iden1");;
-Var(S "iden2");;
+let e1 = Add(Const(1),Const(2));;
+execute ([],gamma,(compile e1)) ;;
+let e2 = Mul(Const(6),Const(6));;
+execute ([],gamma,(compile e2)) ;;
+let e3 = Pow(Const(2),Const(4));;
+execute ([],gamma,(compile e3)) ;;
+let e4 = Div(Const(6),Const(3));;
+execute ([],gamma,(compile e4)) ;;
+let e5 = Var(S "iden1");;
+execute ([],gamma,(compile e5)) ;;
+let e6 = Var(S "iden2");;
+execute ([],gamma,(compile e6)) ;;
 
-Mod(Const(-1));;
-Proj(2,Tuple([Const(12);Const(121);Const(33)]));;
+let e7 = Mod(Const(-1));;
+execute ([],gamma,(compile e7)) ;;
+let e8 = Proj(2,Tuple([Const(12);Const(121);Const(33)]));;
+execute ([],gamma,(compile e8)) ;;
 
-Sub(Proj(2,Tuple[Const(2);Const(5);Const(8)]),Const(1));;
-Mod(Proj(2,Tuple[Const(2);Const(5);Const(8)]));;
 
-Or(
+let e9 = Sub(Proj(2,Tuple[Const(2);Const(5);Const(8)]),Const(1));;
+execute ([],gamma,(compile e9)) ;;
+let e10 = Mod(Proj(2,Tuple[Const(2);Const(5);Const(8)]));;
+execute ([],gamma,(compile e10)) ;;
+
+let e11 =Or(
 	EqI(Const(5),Const(5)),
 	And(EqI(Sub(Const(2),Const(1)),Const(1)),
 		Gt( Mod(Proj(2,Tuple[Const(2);Const(5);Const(8)])),Const(2))
 	)
 );;
+execute ([],gamma,(compile e11)) ;;
 
-And((true), (false));;
-Impl(Not(Impl(Or((true), (false)), And((true), (false)))),Impl(And((true), (false)), Or((true), (false))));;
+let e12 = And((true), (false));;
+execute ([],gamma,(compile e12)) ;;
 
-Gte(Const(4),Const(2));;
-Lte(Const(4),Const(2));;
+let e13 = Impl(Not(Impl(Or((true), (false)), And((true), (false)))),Impl(And((true), (false)), Or((true), (false))));;
+execute ([],gamma,(compile e13)) ;;
+
+let e14 = Gte(Const(4),Const(2));;
+execute ([],gamma,(compile e14)) ;;
+
+let e15 = Lte(Const(4),Const(2));;
+execute ([],gamma,(compile e15)) ;;
 
 (* We weren't even told what was if then else till then *)
 (* Ifthenelse *)
-( Gt(Const(4),Const(2)) , Add(Const(1),Const(3)), Sub(Const(1),Const(3)) );
-
-(* Lambda is a lambda function of type exp*exp and LetinEnd is a ternary operator of type exp*exp*exp *)
-(* Apply( Lambda(Var("x"), LetinEnd(Para[Assgn(Var("a"),Const(2))],Add(Var("a"),Var("x")))),Const(2)) *)
-
-(* We weren't told IFTE and Lambda till then *)
+(* let e16 = ( Gt(Const(4),Const(2)) , Add(Const(1),Const(3)), Sub(Const(1),Const(3)) ); *)
 
 (* 
 Other things that I have implemented are Xor , Max, Min, Var 15 examples given in the code itself
 *)
-(* Automate the checking? *)
