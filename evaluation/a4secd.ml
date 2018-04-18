f5080c3b7d9d44443d2aa219cd920f412466d583
udit01 <uditjainiitd@gmail.com>
2018-04-18 20:38:10 +0530

    SECD demo 2 test cases remaining to sort

--- evaluation/a4secd.ml
+++ evaluation/a4secd.ml
@@ -1,93 +1,30 @@
 (*
  * Note: you need to change the following datatype and expressions as per your submitted code
- * Please do the changes before you come for demo.
+ * Please do the changes before you come for demo. 
  *)
 
-datatype exp = Num of int
-			| Bool of bool
-			| Var of string
-			| List of exp list
-			| Plus of exp * exp
-			| Sub of exp * exp
-			| Mult of exp * exp
-			| Div of exp * exp
-			| Tup of exp list
-			| Proj of exp * int
-			| Gtr of exp * exp
-			| Lsr of exp * exp
-			| Eql of exp * exp
-			| Ifthenelse of exp * exp * exp
-			| Lambda of exp * exp
-			| App of exp * exp
-			| LetinEnd of exp * exp
-			| Assgn of exp * exp
-			| Seq of exp list
-			| Para of exp list
-			| Localinend of exp list * exp
-			| Dec of exp list
-			| Ctup of closure list
-			| At of int
-			| Bind of exp
-			
-			| Restp of exp
-			| Tothisp of exp
-			| Rests of exp
-			| Tothiss of exp
-			| Restm of exp
-			| Tothism of exp
-			| Restd of exp
-			| Tothisd of exp
-			| Restg of exp
-			| Tothisg of exp
-			| Restl of exp
-			| Tothisl of exp
-			| Reste of exp
-			| Tothise of exp
-			| Ifthn of exp * exp
-			| Lets of exp*exp
-			and
-			closure = ACL of (exp * closure) list * exp
+execute( [ Proj( 2, Tup([ C 12 , C 121 , C 33 ]) ) ] ) ;;
+
+(* execute( [ Let ( Para([ Assgn (Var("a"), C 1 ), Assgn (Var("b"), C 2 ), Assgn (Var("c"), C 3 )]), Add ( Add ( V(Var("a")) , V(Var("b")) ), Mul ( V(Var("c")) , C 2 ))) , Mul ( C 2 , C 3 ) ] ) ;; *)
+
+execute( [], [ ( Var("a"), C 1 ),  (Var("b"), C 2 ),  (Var("c"), C 3 ) ] ,  compile(Add ( Add ( V(Var("a")) , V(Var("b")) ), Mul ( V(Var("c")) , C 2 ))) @ compile(Mul ( C 2 , C 3 )) , [] ) ;;
+
+execute( [], [], compile( Let(Var "a", C 1, Let( Var "b", C 2, Let( Var "c", C 3, (Add ( Add ( V(Var("a")) , V(Var("b")) ), Mul ( V(Var("c")) , C 2 ))))))) @ compile( Mul(C 2, C 3) ), [])
+
+execute( []. []. compile( Def([ (Var "a", C 1) ; (Var "b", C 2) ; (Var "a", C 3) ;  ]) )@ compile( Add ( Add ( V(Var("a")) , V(Var("b")) ), Mul ( V(Var("c")) , C 2 ) )) @compile(Mul(C 2, C 3)) , [])
 
 
+execute( [], [] compile( Ifte ( Gt ( C 4 , C 2 ), Add ( C 1 , C 3 ), Sub ( C 1 , C 3 ))) , [] ) ;;
 
-krivine(ACL([(Var("z"),ACL([],Num(3)))],Var("z")),[]);;
+execute( [ Let (Dec([Para([ Assgn (Var("f"),Bool(false))]),Seq([ Assgn (Var("a"), C 1 ), Assgn (Var("b"), C 2 ), Assgn (Var("c"), C 3 )])]), Add ( Add (Var("a"),Var("b")), Mul (Var("c"), C 2 ))), Mul ( C 2 , C 3 )] ) ;;
 
-krivine(ACL([],Plus(Plus(Num(2),Num(3)),Plus(Num(2),Num(3)))),[]);;
+execute( [], [],  compile( Apply ( L(Lambda( Var("x"), Add (Var("x"), C 1 ))) , C 2 ) , [] ) ;;
 
-krivine(ACL([(Var("z"),ACL([],Num(3)))],Plus(Num(2),Var("z"))),[]);;
+execute( [], [] , compile ( Apply ( L(Lambda (Var("x"), Mul (Var("x"), Add (Var("x"), C 1 )))), C 2 )), [] ) ;;
 
-krivine(ACL([],App(Lambda(Var("x"),Plus(Var("x"),Num(1))),Num(2))),[]);;
+execute( [], [] , compile( Apply ( L (Lambda (Var("x"), Apply ( L(Lambda(Var("d"), Mul(V (Var "d") , C 2) )) , C 2 ))) , C 2 )) , [] ) ;;
 
-krivine(ACL([],App(Lambda(Var("x"),Mult(Var("x"),Plus(Var("x"),Num(1)))),Num(2))),[]);;
-
-krivine(ACL([],App(Lambda(Var("x"),App(Lambda(Var("d"),Mult(Var("d"),Num(2))),Num(2))),Num(2))),[]);;
-
-krivine(ACL([],Ifthenelse(Gtr(Num(8),Num(2)),App(Lambda(Var("x"),Div(Var("x"),Num(2))),Num(2)),App(Lambda(Var("x"),Mult(Var("x"),Plus(Var("x"),Num(1)))),Num(2)))),[]);;
-
-krivine(ACL([],Ifthenelse(Gtr(Num(1),Num(2)),App(Lambda(Var("x"),Div(Var("x"),Num(2))),Num(2)),App(Lambda(Var("x"),Mult(Var("x"),Plus(Var("x"),Num(1)))),Num(2)))),[]);;
-
-krivine(ACL([],LetinEnd(Para[Assgn(Var("a"),Num(2))],Plus(Var("a"),Num(20)))),[]);;
-
-krivine(ACL([],LetinEnd(Seq[Assgn(Var("a"),Num(2))],Plus(Var("a"),Num(20)))),[]);;
-
-krivine(ACL([],Proj(Tup([Num(1),Num(2),Num(3)]),2)),[]);;
-
-krivine(ACL([],App(Lambda(Var("x"),LetinEnd(Para[Assgn(Var("a"),Num(2))],Plus(Var("a"),Var("x")))),Num(2))),[]);;
+execute( [Seq([ Assgn (Var("a"), Let (Para([ Assgn (Var("a"), C 1 )]), Apply ( Lambda (Var("x"), Add (Var("x"), C 1 )),Var("a"))))]), Add (Var("a"), C 1 )] );;
 
 
-SECD([Proj(Tup([Num(12),Num(121),Num(33)]),2)]);;
-
-SECD([LetinEnd(Para([Assgn(Var("a"),Num(1)),Assgn(Var("b"),Num(2)),Assgn(Var("c"),Num(3))]),Plus(Plus(Var("a"),Var("b")),Mult(Var("c"),Num(2)))),Mult(Num(2),Num(3))]);;
-
-SECD([Ifthenelse(Gtr(Num(4),Num(2)),Plus(Num(1),Num(3)),Sub(Num(1),Num(3)))]);;
-
-SECD([LetinEnd(Dec([Para([Assgn(Var("f"),Bool(false))]),Seq([Assgn(Var("a"),Num(1)),Assgn(Var("b"),Num(2)),Assgn(Var("c"),Num(3))])]),Plus(Plus(Var("a"),Var("b")),Mult(Var("c"),Num(2)))),Mult(Num(2),Num(3))]);;
-
-SECD([App(Lambda(Var("x"),Plus(Var("x"),Num(1))),Num(2))]);;
-
-SECD([App(Lambda(Var("x"),Mult(Var("x"),Plus(Var("x"),Num(1)))),Num(2))]);;
-
-SECD([App(Lambda(Var("x"),App(Lambda(Var("d"),MultVar("d"),Num(2))),Num(2))),Num(2)]);;
-
-SECD([Seq([Assgn(Var("a"),LetinEnd(Para([Assgn(Var("a"),Num(1))]),App(Lambda(Var("x"),Plus(Var("x"),Num(1))),Var("a"))))]),Plus(Var("a"),Num(1))]);
 
